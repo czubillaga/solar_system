2e5ec71d07db9e6d13a328a0db3e9c93b272ff7d
Lewis Ingram <lewisingram@Lewiss-MacBook-Pro-2.local>
2021-10-23 19:28:39 +0100

    planets_detail

--- solar_system_project/server/db/seeds.js
+++ solar_system_project/server/db/seeds.js
@@ -7,127 +7,136 @@ db.planets.insertMany([
     planet: "True",
     moons: null,
     density: 5.4291,
-    gravity: 3.7
+    gravity: 3.7,
+    image_url: "https://solarsystem.nasa.gov/resources/439/mercurys-subtle-colors/?category=planets_mercury"
   },
   {
     name: "Venus",
     planet: "True",
     moons: null,
     density: 5.243,
-    gravity: 8.87
+    gravity: 8.87,
+    image_url: "https://solarsystem.nasa.gov/resources/2524/newly-processed-views-of-venus-from-mariner-10/?category=planets_venus"
   },
   {
     name: "Earth",
     planet: "True",
     moons: [
       {
-        moon: "The Moon"
+        name: "The Moon",
+        image_url: "https://solarsystem.nasa.gov/resources/843/rare-full-moon-on-christmas-day/?category=moons_earths-moon"
       }
     ],
     density: 5.5136,
-    gravity: 9.8
+    gravity: 9.8,
+    image_url: "https://solarsystem.nasa.gov/resources/581/earth-by-suomi-npp/?category=planets_earth"
   },
   {
     name: "Mars",
     planet: "True",
     moons: [
       {
-        moon: "Phobos"
+        name: "Phobos"
       },
       {
-        moon: "Deimos"
+        name: "Deimos"
       }
     ],
     density: 3.9341,
-    gravity: 3.71
+    gravity: 3.71,
+    image_url: "https://solarsystem.nasa.gov/resources/683/valles-marineris-the-grand-canyon-of-mars/?category=planets_mars"
   },
   {
     name: "Jupiter",
     planet: "True",
     moons: [
       {
-        moon: "Io"
+        name: "Io"
       },
       {
-        moon: "Europa"
+        name: "Europa"
       },
       {
-        moon: "Ganymede"
+        name: "Ganymede"
       },
       {
-        moon: "Callisto"
+        name: "Callisto"
       }
     ],
     density: 1.3262,
-    gravity: 24.79
+    gravity: 24.79,
+    image_url: "https://solarsystem.nasa.gov/resources/803/jupiter-and-io/?category=planets_jupiter"
   },
   {
     name: "Saturn",
     planet: "True",
     moons: [
     {
-      moon: "Titan"
+      name: "Titan"
     },
     {
-      moon: "Rhea"
+      name: "Rhea"
     },
     {
-      moon: "Dione"
+      name: "Dione"
     },
     {
-      moon: "Tethys"
+      name: "Tethys"
     },
     {
-      moon: "Mimas"
+      name: "Mimas"
     }
   ],
     density: 0.6871,
-    gravity: 10.44 
+    gravity: 10.44,
+    image_url: "https://solarsystem.nasa.gov/resources/663/saturn-taken-from-voyager-2/?category=planets_saturn"
   },
   {
     name: "Uranus",
     planet: "True",
     moons: [
       {
-        moon: "Miranda"
+        name: "Miranda"
       },
       {
-        moon: "Ariel"
+        name: "Ariel"
       },
       {
-        moon: "Umbriel"
+        name: "Umbriel"
       },
       {
-        moon: "Titania"
+        name: "Titania"
       },
       {
-        moon: "Oberon"
+        name: "Oberon"
       }
     ],
     density: 1.27,
-    gravity: 8.87
+    gravity: 8.87,
+    image_url: "https://solarsystem.nasa.gov/resources/603/uranus-final-image/?category=planets_uranus"
   },
   {
     name: "Neptune",
     planet: "True",
     moons: [
       {
-        moon: "Triton"
+        name: "Triton"
       },
       {
-        moon: "Hippocamp"
+        name: "Hippocamp"
       },
       {
-        moon: "Proteus"
+        name: "Proteus"
       },
       {
-        moon: "Galatea"
+        name: "Galatea"
       },
       {
-        moon: "Despina"
+        name: "Despina"
       }
     ],
     density: 1.638,
-    gravity: 11.15
+    gravity: 11.15,
+    image_url: "https://solarsystem.nasa.gov/resources/611/neptune-full-disk-view/?category=planets_neptune"
   }
 ]);
