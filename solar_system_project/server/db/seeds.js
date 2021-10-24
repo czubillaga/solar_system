0adc9b66ea24c0d9c801183fe7bbb171341f004a
Lewis Ingram <lewisingram@Lewiss-MacBook-Pro-2.local>
2021-10-24 12:18:50 +0100

    updated details

--- solar_system_project/server/db/seeds.js
+++ solar_system_project/server/db/seeds.js
@@ -4,22 +4,28 @@ db.dropDatabase()
 db.planets.insertMany([
     {
     name: "Mercury",
+    altnames: "",
+    temp: "-173 to 427°C",
     planet: "True",
-    moons: null,
-    density: 5.4291,
-    gravity: 3.7,
+    moons: [],
+    gravity: "3.7g",
+    distance: "46.925 million",
     image_url: "https://solarsystem.nasa.gov/resources/439/mercurys-subtle-colors/?category=planets_mercury"
   },
   {
     name: "Venus",
+    altnames: "",
+    temp: "462°C",
     planet: "True",
-    moons: null,
-    density: 5.243,
-    gravity: 8.87,
+    moons: [],
+    gravity: "8.87g",
+    distance: "108.82 million",
     image_url: "https://solarsystem.nasa.gov/resources/2524/newly-processed-views-of-venus-from-mariner-10/?category=planets_venus"
   },
   {
     name: "Earth",
+    altnames: "Gaia, Terra, Tellus",
+    temp: "-88 to 58°C",
     planet: "True",
     moons: [
       {
@@ -27,12 +33,14 @@ db.planets.insertMany([
         image_url: "https://solarsystem.nasa.gov/resources/843/rare-full-moon-on-christmas-day/?category=moons_earths-moon"
       }
     ],
-    density: 5.5136,
-    gravity: 9.8,
+    gravity: "9.8g",
+    distance: "148.79 million",
     image_url: "https://solarsystem.nasa.gov/resources/581/earth-by-suomi-npp/?category=planets_earth"
   },
   {
     name: "Mars",
+    altnames: "The Red Planet",
+    temp: "-87 to -5°C",
     planet: "True",
     moons: [
       {
@@ -42,12 +50,14 @@ db.planets.insertMany([
         name: "Deimos"
       }
     ],
-    density: 3.9341,
-    gravity: 3.71,
+    gravity: "3.71g",
+    distance: "243.02 million",
     image_url: "https://solarsystem.nasa.gov/resources/683/valles-marineris-the-grand-canyon-of-mars/?category=planets_mars"
   },
   {
     name: "Jupiter",
+    altnames: "Jove, The Gas Giant",
+    temp: "-108°C",
     planet: "True",
     moons: [
       {
@@ -63,12 +73,14 @@ db.planets.insertMany([
         name: "Callisto"
       }
     ],
-    density: 1.3262,
-    gravity: 24.79,
+    gravity: "24.79g",
+    distance: "746.6 million",
     image_url: "https://solarsystem.nasa.gov/resources/803/jupiter-and-io/?category=planets_jupiter"
   },
   {
     name: "Saturn",
+    altnames: "The Ringed Planet",
+    temp: "-139°C",
     planet: "True",
     moons: [
     {
@@ -87,12 +99,14 @@ db.planets.insertMany([
       name: "Mimas"
     }
   ],
-    density: 0.6871,
-    gravity: 10.44,
+    gravity: "10.44g",
+    distance: "1.4836 billion",
     image_url: "https://solarsystem.nasa.gov/resources/663/saturn-taken-from-voyager-2/?category=planets_saturn"
   },
   {
     name: "Uranus",
+    altnames: "The Bulls-Eye Planet",
+    temp: "-197°C",
     planet: "True",
     moons: [
       {
@@ -111,12 +125,14 @@ db.planets.insertMany([
         name: "Oberon"
       }
     ],
-    density: 1.27,
-    gravity: 8.87,
+    gravity: "8.87g",
+    distance: "2.9513 billion",
     image_url: "https://solarsystem.nasa.gov/resources/603/uranus-final-image/?category=planets_uranus"
   },
   {
     name: "Neptune",
+    altnames: "The Planet exterior to Uranus",
+    temp: "-201°C",
     planet: "True",
     moons: [
       {
@@ -135,8 +151,8 @@ db.planets.insertMany([
         name: "Despina"
       }
     ],
-    density: 1.638,
-    gravity: 11.15,
+    gravity: "11.15g",
+    distance: "4.475 billion",
     image_url: "https://solarsystem.nasa.gov/resources/611/neptune-full-disk-view/?category=planets_neptune"
   }
 ]);
