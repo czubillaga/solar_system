f4a3505bae16b215bc812677345330b6b49839f7
Lewis Ingram <lewisingram@Lewiss-MacBook-Pro-2.local>
2021-10-27 08:43:59 +0100

    stlying updataes

--- solar_system_project/client/src/components/PlanetsBackgroundDetail.js
+++ solar_system_project/client/src/components/PlanetsBackgroundDetail.js
@@ -8,49 +8,49 @@ const PlanetsBackgroundDetail = () => {
 
                 <a href="https://www.youtube.com/watch?v=rX_NCCpw5Uo">
                     <div className='mercury'>
-                        <p className='mercury_p'>Click on the planet to learn more!</p>
+                        <p className='mercury_p'>Click on Mercury to learn more!</p>
                     </div>
                 </a>
 
                 <a href="https://www.youtube.com/watch?v=5v5prMW3AzA">
                     <div className='venus'>
-                    <p className='venus_p'>Click on the planet to learn more!</p>
+                    <p className='venus_p'>Click on Venus to learn more!</p>
                     </div>
                 </a>
 
                 <a href="https://www.youtube.com/watch?v=v9CRt3-bns0">
                     <div className='earth'>
-                    <p className='earth_p'>Click on the planet to learn more!</p>
+                    <p className='earth_p'>Click on Earth to learn more!</p>
                     </div>
                 </a>
 
                 <a href="https://www.youtube.com/watch?v=H19V4w2P7fE">
                     <div className='mars'>
-                    <p className='mars_p'>Click on the planet to learn more!</p>
+                    <p className='mars_p'>Click on Mars to learn more!</p>
                     </div>
                 </a>
 
                 <a href="https://www.youtube.com/watch?v=PtkqwslbLY8">
                     <div className='jupiter'>
-                    <p className='jupiter_p'>Click on the planet to learn more!</p>
+                    <p className='jupiter_p'>Click on Jupiter to learn more!</p>
                     </div>
                 </a>
 
                 <a href="https://www.youtube.com/watch?v=vlb-b5ch81k">
                     <div className='saturn'>
-                    <p className='saturn_p'>Click on the planet to learn more!</p>
+                    <p className='saturn_p'>Click on Saturn to learn more!</p>
                     </div>
                 </a>
 
                 <a href="https://www.youtube.com/watch?v=m4NXbFOiOGk">
                     <div className='uranus'>
-                    <p className='uranus_p'>Click on the planet to learn more!</p>
+                    <p className='uranus_p'>Click on Uranus to learn more!</p>
                     </div>
                 </a>
 
                 <a href="https://www.youtube.com/watch?v=NStn7zZKXfE">
                     <div className='neptune'>
-                    <p className='neptune_p'>Click on the planet to learn more!</p>
+                    <p className='neptune_p'>Click on Neptune to learn more!</p>
                     </div>
                 </a>
 
